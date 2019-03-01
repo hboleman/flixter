ca57e41c8ac3a8e91efc8a8e2fc2191597f5b453
Hunter Boleman <hunter.boleman@gmail.com>
2019-02-28 17:03:35 -0800

    Flixter Part 2: 1.2 Nav: DONE

---
+++ flixter/MovieDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailsViewController.swift
+//  flixter
+//
+//  Created by Hunter Boleman on 2/28/19.
+//  Copyright © 2019 Hunter Boleman. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    var movie: [String:Any]!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(movie["title"]);
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- flixter/MovieViewController.swift
+++ flixter/MovieViewController.swift
@@ -71,4 +71,27 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         return cell;
     }
+    
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+        print("loading up details screen...");
+        
+        // Find selected movie
+        let cell = sender as! UITableViewCell;
+        let indexPath = tableView.indexPath(for: cell)!;
+        let movie = movies[indexPath.row];
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController;
+        detailsViewController.movie = movie;
+        
+        // Deselect row before segue
+        tableView.deselectRow(at: indexPath, animated: true);
+        
+     }
+    
 }
