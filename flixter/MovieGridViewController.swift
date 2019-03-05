39de3103f3208f2149da38ccd517c04fd9ef5a9e
Hunter Boleman <hunter.boleman@gmail.com>
2019-03-05 12:52:18 -0800

    Flixter 2: Bonus User Story 1: COMPLETE

--- flixter/MovieGridViewController.swift
+++ flixter/MovieGridViewController.swift
@@ -77,15 +77,31 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         return cell;
     }
     
-
-    /*
+    private func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("Selected cell number: \(indexPath.row)")
+    }
+    
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        print("loading up details screen...");
+        
+        // Find selected movie
+        let cell = sender as! UICollectionViewCell;
+        let indexPath = collectionView.indexPath(for: cell)!;
+        let movie = movies[indexPath.row];
+        
+        // Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController;
+        detailsViewController.movie = movie;
+        
+        // Deselect row before segue
+        collectionView.deselectItem(at: indexPath, animated: true);
+        
     }
-    */
+ 
+    
 
 }
